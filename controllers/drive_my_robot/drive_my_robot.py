094cea2fffa5b0468c9ec145644277ea852b2630
mklapkowski <mklapkow@macalester.edu>
2022-04-25 16:44:56 -0500

    changes to controller for gps and lidar

--- controllers/drive_my_robot/drive_my_robot.py
+++ controllers/drive_my_robot/drive_my_robot.py
@@ -2,16 +2,22 @@
 
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
-from controller import Robot,DistanceSensor
+from controller import Robot,Lidar
 
-TIME_STEP = 64
 robot = Robot()
-
+TIME_STEP = 64
+  
 ds = []
-dsNames = ['sensor_1', 'sensor_2']
+dsNames = ['lidar', 'gps']
 for i in range(2):
     ds.append(robot.getDevice(dsNames[i]))
     ds[i].enable(TIME_STEP)
+        
+lidar = ds[0]
+lidar.enablePointCloud()
+    
+gps = ds[1]
+
     
 wheels = []
 wheelsNames = ['motor_1', 'motor_2']
@@ -19,34 +25,39 @@ for i in range(2):
     wheels.append(robot.getDevice(wheelsNames[i]))
     wheels[i].setPosition(float('inf'))
     wheels[i].setVelocity(0.0)
+        
+    # LeftObstacleCounter = 0
+    # RightObstacleCounter = 0
     
-LeftObstacleCounter = 0
-RightObstacleCounter = 0
-
+   
 while robot.step(TIME_STEP) != -1:
-
+    rangeImage = lidar.getRangeImage()
+    curr = gps.getValues()
+    print(curr)
     leftSpeed = 5.0
     rightSpeed = 5.0
+      
+        
+    # if LeftObstacleCounter > 0:
+        # leftSpeed = -5.0
+        # rightSpeed = 5.0
+        # LeftObstacleCounter -= 10
+    # elif RightObstacleCounter > 0:
+        # leftSpeed = -5.0
+        # rightSpeed = 5.0
+        # RightObstacleCounter -= 10
+    # else:  # read sensors 
+        # if ds[0].getValue() < 950.0:
+            # LeftObstacleCounter = 100
+        # elif ds[1].getValue() < 950.0:
+            # RightObstacleCounter = 100
     
-    if LeftObstacleCounter > 0:
-        leftSpeed = -5.0
-        rightSpeed = 5.0
-        LeftObstacleCounter -= 10
-    elif RightObstacleCounter > 0:
-        leftSpeed = -5.0
-        rightSpeed = 5.0
-        RightObstacleCounter -= 10
-    else:  # read sensors 
-        if ds[0].getValue() < 950.0:
-            LeftObstacleCounter = 100
-        elif ds[1].getValue() < 950.0:
-            RightObstacleCounter = 100
-            
     wheels[0].setVelocity(leftSpeed)
     wheels[1].setVelocity(rightSpeed)
-  
+      
         
-        
+ 
             
-    
-    # Enter here exit cleanup code.
+                
+        
+   
