62568281a41f7af54bfe8d8754bb6613866b798b
mklapkowski <mklapkow@macalester.edu>
2022-05-01 12:24:45 -0500

    a whole bunch of stuff moved into 4wheel

--- controllers/4wheel_controller/4wheel_controller.py
+++ controllers/4wheel_controller/4wheel_controller.py
@@ -1,66 +1,73 @@
 from controller import Robot
 import math
+import routeFinding as rf
 
-
-def initializeRobot(self):
-    self.TIME_STEP = 32
-    self.robot = Robot()
-    self.ds = []
-    dsNames = ['ds_right', 'ds_left', 'gps', 'compass']
-    for i in range(4):
-        self.ds.append(self.robot.getDevice(dsNames[i]))
-        self.ds[i].enable(self.TIME_STEP)
-    wheels = []
-    wheelsNames = ['wheel1', 'wheel2', 'wheel3', 'wheel4']
-    for i in range(4):
-        wheels.append(self.robot.getDevice(wheelsNames[i]))
-        wheels[i].setPosition(float('inf'))
-        wheels[i].setVelocity(0.0)
-
-avoidObstacleCounter = 0
-
-def initLocal(self):
-
-    self.gps = self.ds[2]
-    self.compass = self.ds[3]
-    self.robot.step(self.TIME_STEP)
-
-def findBearing(self):
-    self.north = self.compass.getValues()  
+def findBearing():
+    north = compass.getValues()  
     rad = math.atan2(north[0], north[1])
     bearing = (rad - 1.5708) / math.pi * 180
     if bearing < 0.0:
         bearing = bearing + 360.0
     return bearing
 
-def findCoord(self):
-    return self.gps.getValues()
+def findCoord():
+    return gps.getValues()
 
-def angleBetweenTwoPoints(self, curr, target):
+def angleBetweenTwoPoints(curr, target):
     radianAngle = math.atan2(curr[1] - target[1], curr[0] - target[0])
     degreeAngle = math.degrees(radianAngle) + 90
     if degreeAngle < 0.0:
         degreeAngle = degreeAngle + 360.0
     return degreeAngle
 
-def moveToNextNode(self, turnAngle, target):
-    if turnAngle >= 2:
+def moveToNextNode(turnAngle, target):
+    if turnAngle >= 2.5:
         leftSpeed = -3.0
         rightSpeed = 3.0
-    elif abs(self.gps.getValues()[0] - target[0]) >= 0.5 or abs(self.gps.getValues()[1] - target[1]) >= 0.5:
+    elif abs(gps.getValues()[0] - target[0]) >= 0.5 or abs(gps.getValues()[1] - target[1]) >= 0.5:
         leftSpeed = 5.0
         rightSpeed = 5.0
     else:
         leftSpeed = 0
         rightSpeed = 0
-    self.wheels[0].setVelocity(leftSpeed)
-    self.wheels[1].setVelocity(rightSpeed)
-    self.wheels[2].setVelocity(leftSpeed)
-    self.wheels[3].setVelocity(rightSpeed)
-    
+    wheels[0].setVelocity(leftSpeed)
+    wheels[1].setVelocity(rightSpeed)
+    wheels[2].setVelocity(leftSpeed)
+    wheels[3].setVelocity(rightSpeed)
 
-def run(self):
-    while self.robot.step(self.TIME_STEP) != -1:
+
+def nearestNode(curr):
+    dictOfNodes = rf.getNodes()
+    nearest = 99999
+    nearestName = None
+    for i in dictOfNodes.keys():
+        res = rf.calculateDistance(curr[0], curr[1], dictOfNodes.get(i)[0], dictOfNodes.get(i)[1])
+        if res < nearest:
+            nearest = res
+            nearestName = i
+    return nearestName
+
+def firstMove():
+   
+    curr = findCoord()
+    firstNode = nearestNode(curr)
+    target = rf.dictOfNodes.get(firstNode)
+
+    while abs(gps.getValues()[0] - target[0]) >= 0.5 or abs(gps.getValues()[1] - target[1]) >= 0.5:
+        robot.step(TIME_STEP)
+        newBearing = findBearing()
+        curr = findCoord()
+
+        degreeAngle = angleBetweenTwoPoints(curr, target)
+    
+        turnAngle = abs(newBearing - degreeAngle)
+
+        moveToNextNode(turnAngle, target)
+
+
+
+def run(goal):
+    while robot.step(TIME_STEP) != -1:
         target = (58, -16.3)
         # if avoidObstacleCounter > 0:
             # avoidObstacleCounter -= 1
@@ -72,15 +79,28 @@ def run(self):
                     # avoidObstacleCounter = 100
 
 
-        newBearing = findBearing()
-        curr = findCoord()
+      
             
-        degreeAngle = angleBetweenTwoPoints(curr, target)
-        turnAngle = abs(newBearing - degreeAngle)
-
-        moveToNextNode(turnAngle, target)
-
+        
 
 if __name__=="__main__":
-    initializeRobot()
+    TIME_STEP = 32
+    robot = Robot()
+    ds = []
+    dsNames = ['ds_right', 'ds_left', 'gps', 'compass']
+    for i in range(4):
+        ds.append(robot.getDevice(dsNames[i]))
+        ds[i].enable(TIME_STEP)
+    wheels = []
+    wheelsNames = ['wheel1', 'wheel2', 'wheel3', 'wheel4']
+    for i in range(4):
+        wheels.append(robot.getDevice(wheelsNames[i]))
+        wheels[i].setPosition(float('inf'))
+        wheels[i].setVelocity(0.0)
 
+
+    gps = ds[2]
+    compass = ds[3]
+    robot.step(TIME_STEP)
+    firstMove()
+    # run()
